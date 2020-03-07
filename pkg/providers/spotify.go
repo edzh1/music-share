536ab0e10145bba7b69820ef7ec591c103c78f13
Dmitrii Kovtun <dkovtun.work@gmail.com>
2020-03-07 13:14:20 +0700

    add GetTrack method for spotify provider

--- pkg/providers/spotify.go
+++ pkg/providers/spotify.go
@@ -1,15 +1,52 @@
 package providers
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 type spotifyProvider struct {
 	provider
 }
 
+//Spotify provider
 var Spotify = &spotifyProvider{
 	provider: provider{
-		name: "spotify",
+		name:  "spotify",
+		token: "",
+		endpoints: map[string]string{
+			"GET_TRACK": "https://api.spotify.com/v1/tracks",
+			"GET_ALBUM": "https://api.spotify.com/v1/albums",
+			"SEARCH":    "https://api.spotify.com/v1/search",
+			"AUTH":      "https://accounts.spotify.com/api/token",
+		},
 	},
 }
 
-func (p *spotifyProvider) getTrack(string) string {
-	return "1"
+func (p *spotifyProvider) GetTrack(trackID string) string {
+	url := fmt.Sprintf("%s/%s", Spotify.endpoints["GET_TRACK"], trackID)
+	request, err := http.NewRequest("GET", url, nil)
+	request.Header.Set("Authorization", p.token)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(body)
 }
