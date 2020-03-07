a77ca4038b86896721eee3afbbe758b8f208c0bd
Dmitrii Kovtun <dkovtun.work@gmail.com>
2020-03-07 17:32:42 +0700

    get link type

--- pkg/urlparser/urlparser.go
+++ pkg/urlparser/urlparser.go
@@ -12,20 +12,51 @@ type URLParser struct {
 
 // TODO: configurable providers
 // TODO: make regexps more accurate
-var regexpMap = map[string]*regexp.Regexp{
-	"yandex":  regexp.MustCompile("music\\.yandex\\.ru"),
-	"spotify": regexp.MustCompile("open\\.spotify\\.com"),
+var regexpMap = map[string]map[string]*regexp.Regexp{
+	"yandex": {
+		"domain": regexp.MustCompile("music\\.yandex\\.ru"),
+		"track":  regexp.MustCompile("/album/\\d+/track/\\d+"),
+		"album":  regexp.MustCompile("/album/\\d+($|\\?)"),
+	},
+	"spotify": {
+		"domain": regexp.MustCompile("open\\.spotify\\.com"),
+		"track":  regexp.MustCompile("/track/\\w+"),
+		"album":  regexp.MustCompile("/album/\\w+"),
+	},
 }
 
 //GetProvider returns provider from an url string
 func (p *URLParser) GetProvider(url string) (string, error) {
 	for _, provider := range p.Providers {
-		pattern := regexpMap[provider]
+		pattern := regexpMap[provider]["domain"]
 
 		if pattern.MatchString(url) {
 			return provider, nil
 		}
 	}
 
-	return "", errors.New("unknown provider")
+	return "", errors.New("URLParser: unknown provider")
+}
+
+//GetLinkType returns link type (track, album, etc) from an url string
+func (p *URLParser) GetLinkType(url string) (string, error) {
+	provider, err := p.GetProvider(url)
+
+	if err != nil {
+		return "", err
+	}
+
+	pattern := regexpMap[provider]["track"]
+
+	if pattern.MatchString(url) {
+		return "track", nil
+	}
+
+	pattern = regexpMap[provider]["album"]
+
+	if pattern.MatchString(url) {
+		return "album", nil
+	}
+
+	return "", errors.New("URLParser: unknown link type")
 }
