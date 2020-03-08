33298c5e450f136b9febcc7a4249137fc6d8147e
Dmitrii Kovtun <dkovtun.work@gmail.com>
2020-03-08 14:22:52 +0700

    add getArtist method for yandex

--- pkg/providers/yandex.go
+++ pkg/providers/yandex.go
@@ -16,15 +16,16 @@ var Yandex = &yandexProvider{
 	provider: provider{
 		Name: "yandex",
 		endpoints: map[string]string{
-			"GET_TRACK": "https://music.yandex.ru/handlers/track.jsx",
-			"GET_ALBUM": "https://music.yandex.ru/handlers/album.jsx",
-			"SEARCH":    "https://music.yandex.ru/handlers/music-search.jsx",
+			"GET_TRACKS":  "https://music.yandex.ru/handlers/track.jsx",
+			"GET_ALBUMS":  "https://music.yandex.ru/handlers/album.jsx",
+			"GET_ARTISTS": "https://music.yandex.ru/handlers/artist.jsx",
+			"SEARCH":      "https://music.yandex.ru/handlers/music-search.jsx",
 		},
 	},
 }
 
 func (p *yandexProvider) GetTrack(trackID string) string {
-	url := fmt.Sprintf("%s?track=%s&lang=en", p.endpoints["GET_TRACK"], trackID)
+	url := fmt.Sprintf("%s?track=%s&lang=en", p.endpoints["GET_TRACKS"], trackID)
 	request, err := http.NewRequest("GET", url, nil)
 
 	if err != nil {
@@ -49,9 +50,33 @@ func (p *yandexProvider) GetTrack(trackID string) string {
 }
 
 func (p *yandexProvider) GetAlbum(albumID string) string {
-	url := fmt.Sprintf("%s?album=%s&lang=en", p.endpoints["GET_ALBUM"], albumID)
+	url := fmt.Sprintf("%s?album=%s&lang=en", p.endpoints["GET_ALBUMS"], albumID)
 	request, err := http.NewRequest("GET", url, nil)
-	request.Header.Set("authorization", p.apiToken)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(body)
+}
+
+func (p *yandexProvider) GetArtist(artistID string) string {
+	url := fmt.Sprintf("%s?artist=%s&lang=en", p.endpoints["GET_ALBUMS"], artistID)
+	request, err := http.NewRequest("GET", url, nil)
 
 	if err != nil {
 		log.Fatal(err)
