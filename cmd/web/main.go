019151d90df0a86b6b757f5bfa149cdeb47f8998
Dmitriy Kovtun <dkovtun.work@gmail.com>
2020-03-23 20:31:20 +0300

    main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,41 +13,70 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+type providerMap map[string]providers.ProviderInterface
 
-	var track = &mongoModels.TrackModel{Client: client}
+type application struct {
+	tracks *mongoModels.TrackModel
+	albums *mongoModels.AlbumModel
+	//artists    *mongoModels.ArtistModel
+	providers providerMap
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	defer cancel()
+
 	// track.Insert("Technoid", "7CpFDGlrIfHLBmCBPvTSLU")
 	// track.Get("7CpFDGlrIfHLBmCBPvTSLU")
+
+	var app = &application{
+		tracks: &mongoModels.TrackModel{Client: client},
+		albums: &mongoModels.AlbumModel{Client: client},
+		providers: providerMap{
+			"spotify": providers.Spotify,
+			"yandex":  providers.Yandex,
+		},
+	}
+
 	// track.Delete("7CpFDGlrIfHLBmCBPvTSLU")
 
 	spotifyCredntials := flag.String("spotifyCredntials", "", "Base64 encoded client_id:clent_secret")
+	providers.Spotify.ClientToken = *spotifyCredntials
 	flag.Parse()
 
 	providerParser := &urlparser.URLParser{
 		Providers: []string{"yandex", "spotify"},
 	}
 
-	provider, err := providerParser.GetProvider("https://open.spotify.com/track/7rqWfVnNo2hyCpSpCpEYFj?si=GHCzKcKgTeKlRgzXDRdw8w")
+	provider, err := providerParser.GetProvider("https://open.spotify.com/track/4DZBk2qmeAvZtSmhSayaXh")
+	// provider, err := providerParser.GetProvider("https://music.yandex.ru/album/10073206/track/63360735")
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// _, _ = app.getTrack("28358063", app.providers[provider])
+	// _, _ = app.getAlbum("9899790", app.providers[provider])
+	// _, _ = app.getArtist("1768379", app.providers[provider])
+	_, _ = app.getTrack("4DZBk2qmeAvZtSmhSayaXh", app.providers[provider])
+	// _, _ = app.getAlbum("7CpFDGlrIfHLBmCBPvTSLU?si=GHCzKcKgTeKlRgzXDRdw8w", app.providers[provider])
+	// _, _ = app.getArtist("24eQxPRLv3UMwEIo6mawVW?si=GHCzKcKgTeKlRgzXDRdw8w", app.providers[provider])
 
 	linkType, err := providerParser.GetLinkType("https://open.spotify.com/track/7rqWfVnNo2hyCpSpCpEYFj?si=GHCzKcKgTeKlRgzXDRdw8w")
+	// linkType, err := providerParser.GetLinkType("https://music.yandex.ru/album/10073206/track/63360735")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	providers.Spotify.ClientToken = *spotifyCredntials
-	// message := providers.Spotify.GetTrack("7rqWfVnNo2hyCpSpCpEYFj")
+	// providers.Spotify.Auth()
+	// message, _ := providers.Spotify.GetTrack("7rqWfVnNo2hyCpSpCpEYFj")
 	// message := providers.Spotify.GetAlbum("7CpFDGlrIfHLBmCBPvTSLU")
 	// message := providers.Yandex.GetTrack("62650884%3A9899790")
 	// message := providers.Spotify.GetAlbum("7CpFDGlrIfHLBmCBPvTSLU")
-	// message := providers.Spotify.Auth()
 
+	// println(message)
 	println(provider)
 	println(linkType)
 }
